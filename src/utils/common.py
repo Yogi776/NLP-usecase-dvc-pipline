34e09cbe6bbfcf00f1857e61963306448a0ad191
yogesh <yogeshkhangode.99@gmail.com>
2022-08-31 13:30:22 +0530

    stage 01

--- src/utils/common.py
+++ src/utils/common.py
@@ -12,3 +12,7 @@ def read_yaml(path_to_yaml: str) -> dict:
     return content
 
 
+def create_directories(path_to_directories: list) -> None:
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        logging.info(f"created directory at: {path}")
